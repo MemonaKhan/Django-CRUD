0753ee8b42077c3f9d84ea0e63932c48d0ceff32
MemonaKhan <suhail1975pk@yahoo.co.uk>
2021-02-23 11:24:09 +0500

    created model

--- student_crud/enroll/urls.py
+++ student_crud/enroll/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
+    path('', views.index, name="index"),
+    path('', views.add, name="add"),
+    path('', views.update, name="update"),
+    path('', views.delete, name="delete"),
     
 ]
--- student_crud/enroll/views.py
+++ student_crud/enroll/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'enroll/index.html')
+    # return render(request, 'tasks/list.html', context)
+
+def add(request):
+    return render(request, 'enroll/add.html')
+
+def update(request):
+    return render(request, 'enroll/edit.html')
+
+
+def delete(request):
+    return render(request, 'enroll/delete.html')
